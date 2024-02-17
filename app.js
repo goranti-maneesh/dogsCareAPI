0b85dafe34cca08bc704c8d98a7bb99bd102b45b
goranti-maneesh <54623814+goranti-maneesh@users.noreply.github.com>
2024-02-17 17:03:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ app.use((req, res, next) => {
 
 // Mock data
 const productsData = [ {
-    image1: "https://res.cloudinary.com/difbmfdoo/image/upload/v1708168010/blob_2_cwi6gi.png"
-    image2: "https://res.cloudinary.com/difbmfdoo/image/upload/v1708168010/Ellipse_2_mo0png.png"
-    image3: "https://res.cloudinary.com/difbmfdoo/image/upload/v1708168009/Ellipse_7_o0aiz4.png"
-    image4: "https://res.cloudinary.com/difbmfdoo/image/upload/v1708168009/Ellipse_4_nki2b8.png"
-    image5 : "https://res.cloudinary.com/difbmfdoo/image/upload/v1708168009/Ellipse_3_y3axzy.png"
-    text_title: "Taking care for your Smart Dog !"
+    image1: "https://res.cloudinary.com/difbmfdoo/image/upload/v1708168010/blob_2_cwi6gi.png",
+    image2: "https://res.cloudinary.com/difbmfdoo/image/upload/v1708168010/Ellipse_2_mo0png.png",
+    image3: "https://res.cloudinary.com/difbmfdoo/image/upload/v1708168009/Ellipse_7_o0aiz4.png",
+    image4: "https://res.cloudinary.com/difbmfdoo/image/upload/v1708168009/Ellipse_4_nki2b8.png",
+    image5 : "https://res.cloudinary.com/difbmfdoo/image/upload/v1708168009/Ellipse_3_y3axzy.png",
+    text_title: "Taking care for your Smart Dog !",
     text_summary: "Human-canine bonding is the relationship between dogs and humans"
 }];
 
